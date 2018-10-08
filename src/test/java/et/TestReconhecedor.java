12426c364af8d28de5d33216881134d9acdd9af5
Hallan Medeiros <hmhallan@gmail.com>
2018-10-07 23:39:37 -0300

    refactoring nos testes

--- src/test/java/et/TestReconhecedor.java
+++ src/test/java/et/TestReconhecedor.java
@@ -5,29 +5,29 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.List;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import et.util.InputParser;
 
 public class TestReconhecedor  {
+	
+	List<Problema> problemas;
+	
+	@Before
+	public void setUp() throws FileNotFoundException, IOException {
+		InputParser parser = new InputParser();
+		this.problemas = parser.parseInput( new FileReader("src/main/resources/et.in")  );
+	}
 
 	@Test
-	public void testa_parser_de_entrada() throws FileNotFoundException, IOException {
-		
-		InputParser parser = new InputParser();
-		
-		List<Problema> problemas = parser.parseInput( new FileReader("src/main/resources/et.in")  );
+	public void testa_parser_de_entrada() {
 		problemas.forEach(System.out::println);
 		
 	}
 	
 	@Test
-	public void testa_primeiro_problema() throws FileNotFoundException, IOException {
-		
-		InputParser parser = new InputParser();
-		
-		List<Problema> problemas = parser.parseInput( new FileReader("src/main/resources/et.in")  );
-		
+	public void testa_primeiro_problema()  {
 		Problema p = problemas.get(0);
 		System.out.println( p );
 		
@@ -38,12 +38,7 @@ public class TestReconhecedor  {
 	}
 	
 	@Test
-	public void testa_segundo_problema() throws FileNotFoundException, IOException {
-		
-		InputParser parser = new InputParser();
-		
-		List<Problema> problemas = parser.parseInput( new FileReader("src/main/resources/et.in")  );
-		
+	public void testa_segundo_problema() {
 		Problema p = problemas.get(1);
 		System.out.println( p );
 		
@@ -54,12 +49,7 @@ public class TestReconhecedor  {
 	}
 	
 	@Test
-	public void testa_terceiro_problema() throws FileNotFoundException, IOException {
-		
-		InputParser parser = new InputParser();
-		
-		List<Problema> problemas = parser.parseInput( new FileReader("src/main/resources/et.in")  );
-		
+	public void testa_terceiro_problema() {
 		Problema p = problemas.get(2);
 		System.out.println( p );
 		
@@ -70,12 +60,7 @@ public class TestReconhecedor  {
 	}
 	
 	@Test
-	public void testa_quarto_problema() throws FileNotFoundException, IOException {
-		
-		InputParser parser = new InputParser();
-		
-		List<Problema> problemas = parser.parseInput( new FileReader("src/main/resources/et.in")  );
-		
+	public void testa_quarto_problema() {
 		Problema p = problemas.get(3);
 		System.out.println( p );
 		
@@ -86,12 +71,7 @@ public class TestReconhecedor  {
 	}
 	
 	@Test
-	public void testa_quinto_problema() throws FileNotFoundException, IOException {
-		
-		InputParser parser = new InputParser();
-		
-		List<Problema> problemas = parser.parseInput( new FileReader("src/main/resources/et.in")  );
-		
+	public void testa_quinto_problema() {
 		Problema p = problemas.get(4);
 		System.out.println( p );
 		
